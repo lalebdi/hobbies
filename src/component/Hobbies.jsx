b4fc5d8d0d08c23e8e0494a6884d42ae31272264
Loai Alebdi <loaialebdi@Loais-MBP.fios-router.home>
2020-07-09 16:23:11 -0400

    foundation

---
+++ src/component/Hobbies.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Hobbies extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Hobbies
