85adc5e0b4320d57c6a0d716c4ce9ffaa179cfa7
Loai Alebdi <loaialebdi@Loais-MBP.fios-router.home>
2020-07-10 14:55:55 -0400

    almost done with the content

---
+++ src/assets/hobbiesArray.js
@@ -0,0 +1,16 @@
+let hobbies =[
+    {
+        name: "Running",
+        img: "./running.jpg"
+    },
+    {
+        name: "Cooking",
+        img: "./cooking.jpg"
+    },
+    {
+        name: "Baking",
+        img: "./baking.jpg"
+    }
+];
+
+export default hobbiesArray;
