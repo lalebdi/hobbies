848780c8f9938d171733744b45260ed490fd7aaf
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-12-14 22:19:14 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -63,7 +63,6 @@ function registerValidSW(swUrl, config) {
               }
             } else {
               console.log('Content is cached for offline use.');
-
   
               if (config && config.onSuccess) {
                 config.onSuccess(registration);
