05e47e09116d9235bde68c902052b2dcf5d82220
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-12-14 22:18:13 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -100,7 +100,7 @@ function checkValidServiceWorker(swUrl, config) {
           });
         });
       } else {
-        // Service worker found. Proceed as normal.
+
         registerValidSW(swUrl, config);
       }
     })
