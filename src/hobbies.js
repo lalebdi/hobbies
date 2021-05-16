f02d30b1e7e335488d703a4538651aaac1f4c74f
lalebdi <67381036+lalebdi@users.noreply.github.com>
2021-05-15 20:59:30 -0400

    Update hobbies.js

--- src/hobbies.js
+++ src/hobbies.js
@@ -1,7 +1,7 @@
 const hobbies =[
     {
         name: "Running",
-        img: "https://images.unsplash.com/photo-1509833903111-9cb142f644e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2455&q=80",
+        img: "https://images.unsplash.com/photo--9cb142f644e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2455&q=80",
         description: "Best stress reliever"
     },
     {
